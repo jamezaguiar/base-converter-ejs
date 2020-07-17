caf624403f9782d5741227d119064e27a979e188
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-07-17 14:24:48 -0300

    Rotas para as páginas de conversão

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,18 @@ import { Router } from 'express';
 
 const routes = Router();
 
+// Rotas de renderização das páginas
 routes.get('/', (request, response) => {
   return response.render('index');
+});
+routes.get('/bin', (request, response) => {
+  return response.render('bin');
+});
+routes.get('/oct', (request, response) => {
+  return response.render('oct');
+});
+routes.get('/hex', (request, response) => {
+  return response.render('hex');
 });
 
 export default routes;
